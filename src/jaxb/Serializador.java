870c34b27ff749522947eff8fca8a273c423f310
napoleao_onaya <napoleao.onaya@gmail.com>
2016-03-10 09:53:51 -0300

    JAXB
    Exemplo de uso do JAXB

---
+++ src/jaxb/Serializador.java
@@ -0,0 +1,28 @@
+package jaxb;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+public class Serializador {
+
+	public static void main(String[] args) throws JAXBException {
+
+		JAXBContext context = JAXBContext.newInstance(Conta.class);
+		Marshaller marshaller = context.createMarshaller();
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Napoleão Kazu Onaya");
+		
+		Conta conta = new Conta();
+		conta.setLimite(2000);
+		conta.setSaldo(2000);
+		conta.setCliente(cliente);
+		
+		marshaller.marshal(conta, new File("conta.xml"));
+		
+	}
+
+}
